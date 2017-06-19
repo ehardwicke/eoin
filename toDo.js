64f42f5c08f6008c043a1c975b7f9840bfcbb89f
ehardwicke <ehardwicke@gmail.com>
2017-06-19 20:46:04 +0100

    Update toDo.js

--- toDo.js
+++ toDo.js
@@ -1,13 +1,15 @@
-angular.module('todoApp', [])
-  .controller('todoListController', function() {
+angular.module('todoApp', []) //Declaring module
+  .controller('todoListController', function() { //creating controller
     var todoList = this;
-    todoList.todos = [];
+    todoList.todos = []; //Initialise empty list
  
+ //function for creating todo list items
     todoList.addTodo = function() {
       todoList.todos.push({text:todoList.todoText, done:false});
       todoList.todoText = '';
     };
  
+ //function for displayin the total and checked amount of todos
     todoList.remaining = function() {
       var count = 0;
       angular.forEach(todoList.todos, function(todo) {
@@ -15,7 +17,7 @@ angular.module('todoApp', [])
       });
       return count;
     };
- 
+ //function for removing ticked boxes via archive link
     todoList.archive = function() {
       var oldTodos = todoList.todos;
       todoList.todos = [];
